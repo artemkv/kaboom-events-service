d0641e8d6796d18b3ed6018d060ff5a8aa4a46d4
artemkv <artemkv@gmail.com>
2019-01-18 16:50:23 +0100

    Set NODE_ENV=test when running unit tests

--- connectorprovider.js
+++ connectorprovider.js
@@ -1,8 +1,7 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
-const kafkaConnector = (process.env.RUN_MODE || 'prod') === 'test' ?
-    require('./kafkaconnectormock') : require('./kafkaconnector');
+const kafkaConnector = process.env.NODE_ENV === 'test' ? require('./kafkaconnectormock') : require('./kafkaconnector');
 const getKafkaConnector = function getKafkaConnector() {
     return kafkaConnector;
 }
