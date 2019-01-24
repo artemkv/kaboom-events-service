f1bccca1ca15987a530ce39a59df942abcc2a053
artemkv <artemkv@gmail.com>
2019-01-24 12:25:45 +0100

    Add custom logging

--- app.js
+++ app.js
@@ -6,8 +6,11 @@ const favicon = require('serve-favicon');
 const restStats = require('@artemkv/reststats');
 const errorHandler = require('@artemkv/errorhandler');
 const myRequest = require('@artemkv/myrequest');
+const requestId = require('./requestid');
+const requestTime = require('./requesttime');
 const version = require('./myversion');
 const eventController = require('./eventcontroller');
+const logger = require('./logger');
 
 dotenv.config();
 
@@ -15,17 +18,25 @@ let server = connect();
 
 server
     .use(restStats.countRequest)
+    .use(requestTime)
+    .use(requestId)
 
     // favicon
     .use(favicon('./favicon.ico'))
-    
+
     // Used for testing / health checks
     .use('/error', errorHandler.handleError)
     .use('/resterror', errorHandler.handleRestError)
 
     // Assemble my request
     .use(myRequest)
-    
+
+    // Log session
+    .use(function (req, res, next) {
+        logger.logSession(req.my.path);
+        return next();
+    })
+
     // Statistics endpoint
     .use('/stats', restStats.getStats)
 
@@ -33,6 +44,11 @@ server
     .use('/event', eventController.postEvent)
 
     // Handles errors
+    .use(function (err, req, res, next) {
+        console.log(err);
+        logger.logFailedRequest(req, res, err);
+        next(err);
+    })
     .use(errorHandler.handle404)
     .use(errorHandler.catchAll);
 
@@ -43,5 +59,7 @@ let ip = env.NODE_IP || 'localhost';
 server.listen(port, ip, function () {
     console.log('Application started');
     console.log('http://' + ip + ":" + port + '/');
+    logger.initialize();
+    logger.log('Application started: http://' + ip + ":" + port + '/');
     restStats.initialize(version);
 });
